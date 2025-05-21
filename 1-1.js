f70e4d8b3ff29967e3c23951bf86837d502d9f44
Arira-re <reukiowequ723@gmail.com>
2025-05-21 12:05:08 +0900

    1-2-03

--- 1-1.js
+++ 1-1.js
@@ -1,4 +1,11 @@
 var startButton = document.getElementsByClassName("startButton")[0];
+var display = document.getElementsByClassName("display")[0];
 startButton.addEventListener("click", function() {
     console.log("start");
+    var seconds=0;
+    setInterval(function() {
+        seconds++;
+        display.innerText = seconds;
+        console.log(seconds);
+    }, 1000);
 });
