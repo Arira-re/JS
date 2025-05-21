5c8fa2a8878b746ccccae10d7dbb77fb8ddf781e
Arira-re <reukiowequ723@gmail.com>
2025-05-21 11:29:52 +0900

    0-3-02まで

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ element.innerHTML=`<strong>JavaScript</strong>で書きました`;
 const button = document.getElementById("btn");
 button.addEventListener("click", function() {
     var numberEle = document.getElementById("number");
-    var val=parseInt(val);
+    var val = numberEle.value;
+    var num=parseInt(val);
     if(num%2==0){
         alert("偶数です");
     }
@@ -12,3 +13,10 @@ button.addEventListener("click", function() {
         alert("偶数ではありません");
     }
 });
+var fruits=[`りんご`,`ばなな`,`みかん`];
+var fruitsStr=``;
+for(var i=0;i<fruits.length;i++){
+    fruitsStr+=`<li class=fruit>`+fruits[i]+`</li>`;
+}
+var arrayElm=document.getElementById(`arrayTest`);
+arrayElm.innerHTML=fruitsStr;
