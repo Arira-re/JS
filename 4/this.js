1c31602c1c37cfe4c38f06a52138bfab0f835e78
Arira-re <reukiowequ723@gmail.com>
2025-05-28 10:27:20 +0900

    ここはやる気なくなったからここまででスキップ

--- 4/this.js
+++ 4/this.js
@@ -25,3 +25,123 @@ obj1.test();
 // => true
 
 
+class MyClass {
+    constructor() {
+        this.name = 'これはMyClassです';
+    }
+
+    test() {
+        console.log(this);
+        console.log(this === instance1);
+    }
+}
+
+const instance1 = new MyClass();
+console.log(instance1); //=> MyClass { name: "これはMyClassです" }
+
+instance1.test();
+// => MyClass { name: "これはMyClassです" }
+// => true
+
+console.log('> 「呼び出された時」の所有者のサンプル');
+const obj2 = {
+    name: 'これはobj2です'
+};
+
+obj2.test = obj1.test; // obj1の関数の参照をobj2に代入
+
+obj2.test();
+
+
+console.log('> callのサンプル');
+
+obj1.test.call(obj2); // obj1.testを呼んでいますが、これのthisをobj2に差し替えて実行します
+
+
+// グローバルオブジェクト
+console.log('> グローバルオブジェクトのサンプル');
+
+// 何も関数に囲まれていないグローバルスコープのthisはグローバルオブジェクトです
+console.log(this === window); // => true
+
+function globalTest() {
+    console.log(this === window); // => true
+}
+
+// オブジェクトに所有されていないのでthisはwindowです
+globalTest();
+
+/////////////
+console.log('> アロー関数のサンプル');
+
+const objArrow = {
+    name: 'これはobjArrowです',
+    test: function () {
+        console.log('testの中です');
+        console.log(this);
+
+        const arrow = () => {
+            console.log('arrowの中です');
+            console.log(this); // => {name: "これはobjArrowです", test: ƒ}
+            console.log(this === objArrow); // => true
+        };
+
+        const normal = function () {
+            console.log('normalの中です');
+            console.log(this); // => Window
+            console.log(this === objArrow); // => false
+        };
+
+        arrow();
+        normal();
+    }
+};
+
+console.log(objArrow); //=> {name: "これはobjArrowです", test: ƒ}
+
+objArrow.test();
+
+
+// アロー関数が無かった頃の書き方
+const legacyObj = {
+    name: '通常関数の場合',
+    test: function () {
+        const self = this;
+        document.body.addEventListener('click', function () {
+            console.log(self.name);
+        });
+    }
+};
+
+legacyObj.test();
+
+// アロー関数によってシンプルに書けるようになりました
+const arrowObj = {
+    name: 'アロー関数の場合',
+    test: function () {
+        document.body.addEventListener('click', () => {
+            console.log(this.name); // これで適切にアクセスできます
+        });
+    }
+};
+
+arrowObj.test();
+
+/////////////
+console.log('> コンストラクタのサンプル');
+
+function MyClass2() {
+    this.name = 'これはMyClass2です';
+    console.log(this);
+}
+
+MyClass2.prototype.test = function () {
+    console.log(this === instance2); // => true
+    console.log('test!');
+};
+
+const instance2 = new MyClass2();
+instance2.test();
+
+
+
